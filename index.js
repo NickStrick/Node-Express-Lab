0f4aa86612c40ad8a3ab70e15703b0687b749126
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-01-08 15:03:53 -0600

    post and delete endpoints working

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const db = require('./data/db.js');
 
 const server = express();
 
-// add your server code starting here
+server.use(express.json());
 
-server.get('/', (req, res) =>{
+// add your server code starting here
+//get posts
+server.get('/api/posts', (req, res) =>{
     db.find().then(posts => {
-        res.status(418).json(posts);
+        res.status(200).json(posts);
     }).catch(err => {
         res.json(err);
     })
     
 });
 
+// get post by id
 server.get('/api/posts/:id', (req, res) => {
     const id = req.params.id;
 
@@ -31,5 +34,37 @@ server.get('/api/posts/:id', (req, res) => {
     .catch(err => res.status(500).json(err))
 });
 
+server.post('/api/posts', (req, res) => {
+    const postInfo = req.body;
+
+    console.log('body:', req.body);
+
+    db.insert(postInfo).then(result => {
+        db.findById(result.id).then( post => {
+            res.status(201).json(post);
+        })
+        .catch( err => res.status(500).json({ message: 'the get by id failed', error: err}) 
+    )
+    .catch(err => res.status(500).json({ message: 'the post failed', error: err }))
+})
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+    .then(post => {
+        if(post){
+            db.remove(id)
+                .then(count => {
+                    res.status(202).json(post);
+                });
+        }else{
+            res.status(404).json({message: 'user with id does not exist.'});
+        }
+    })
+    .catch(err => res.status(500).json(err))
+})
+
+
 server.listen(5000, () => console.log('server running'));
-//this thing
+//hello
